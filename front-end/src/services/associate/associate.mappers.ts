0044e60fb5482b84e12931b3b0969e43d73dadf7
Randson-Silva <randsonalves7@gmail.com>
2026-05-24 12:07:37 -0300

    fix(associates-table): associate status and filters

--- front-end/src/services/associate/associate.mappers.ts
+++ front-end/src/services/associate/associate.mappers.ts
@@ -1,32 +1,26 @@
 import type {
-    AssociateProfileForm,
-    AssociateResponse,
-    CreateAssociatePayload,
-    UpdateAssociatePayload,
+  AssociateProfileForm,
+  AssociateResponse,
+  CreateAssociatePayload,
+  UpdateAssociatePayload,
 } from './associate.types';
 
 export const mapAssociateResponseToForm = (
   associate: AssociateResponse
 ): AssociateProfileForm => ({
   id: associate.id,
-
   fullName: associate.user.name ?? '',
   cpf: associate.cpf ?? '',
   email: associate.user.email ?? '',
-  phone: associate.phone ?? '',
-
+  phone: associate.phone ?? associate.user.phone ?? '',
   birthDate: associate.birthDate ?? '',
-
   category: associate.workCategory ?? '',
-
   addressZipCode: associate.address?.postalCode ?? '',
   addressState: associate.address?.state ?? '',
   addressCity: associate.address?.city ?? '',
   addressNeighborhood: associate.address?.neighborhood ?? '',
   addressStreet: associate.address?.street ?? '',
-
   addressNumber: associate.address?.number ?? '',
-
   race: associate.selfDeclaration?.race ?? '',
   gender: associate.selfDeclaration?.gender ?? '',
   sexualOrientation: associate.selfDeclaration?.sexualOrientation ?? '',
@@ -38,28 +32,24 @@ export const mapAssociateResponseToForm = (
 export const mapFormToUpdatePayload = (
   form: AssociateProfileForm
 ): UpdateAssociatePayload => ({
-  cpf: form.cpf,
-  birthDate: form.birthDate,
-  phone: form.phone,
-
+  cpf: form.cpf || undefined,
+  birthDate: form.birthDate || undefined,
+  phone: form.phone || undefined,
   workCategory: form.category === '' ? undefined : form.category,
-
-  fullName: form.fullName,
-  email: form.email,
-
-  postalCode: form.addressZipCode,
-  street: form.addressStreet,
-  number: form.addressNumber,
-  neighborhood: form.addressNeighborhood,
-  city: form.addressCity,
-  state: form.addressState,
-
-  race: form.race,
-  gender: form.gender,
-  sexualOrientation: form.sexualOrientation,
-  education: form.education || undefined,
-  income: form.income || undefined,
-  disability: form.disability,
+  fullName: form.fullName || undefined,
+  email: form.email || undefined,
+  postalCode: form.addressZipCode || undefined,
+  street: form.addressStreet || undefined,
+  number: form.addressNumber || undefined,
+  neighborhood: form.addressNeighborhood || undefined,
+  city: form.addressCity || undefined,
+  state: form.addressState || undefined,
+  race: form.race || undefined,
+  gender: form.gender || undefined,
+  sexualOrientation: form.sexualOrientation || undefined,
+  education: form.education === '' ? undefined : form.education,
+  income: form.income === '' ? undefined : form.income,
+  disability: form.disability || undefined,
 });
 
 export const mapFormToCreatePayload = (
@@ -69,29 +59,22 @@ export const mapFormToCreatePayload = (
   baseData: {
     email: form.email,
     password,
-
     fullName: form.fullName,
-
     cpf: form.cpf,
-
     phone: form.phone,
   },
-
   birthDate: form.birthDate,
-
   workCategory: form.category === '' ? 'OUTRO' : form.category,
-
   postalCode: form.addressZipCode,
   street: form.addressStreet,
   number: form.addressNumber,
   neighborhood: form.addressNeighborhood,
   city: form.addressCity,
   state: form.addressState,
-
-  race: form.race,
-  gender: form.gender,
-  sexualOrientation: form.sexualOrientation,
-  education: form.education || undefined,
-  income: form.income || undefined,
-  disability: form.disability,
+  race: form.race || undefined,
+  gender: form.gender || undefined,
+  sexualOrientation: form.sexualOrientation || undefined,
+  education: form.education === '' ? undefined : form.education,
+  income: form.income === '' ? undefined : form.income,
+  disability: form.disability || undefined,
 });
