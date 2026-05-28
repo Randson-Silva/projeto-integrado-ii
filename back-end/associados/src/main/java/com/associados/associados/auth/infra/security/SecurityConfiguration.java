86051ca9cdc40f223a355cd10348b38b4f0f4957
Pablo Vinícios <pablo.araujo@alu.ufc.br>
2026-05-28 08:20:13 -0300

    fix: remove unused import statements in SecurityConfiguration

--- back-end/associados/src/main/java/com/associados/associados/auth/infra/security/SecurityConfiguration.java
+++ back-end/associados/src/main/java/com/associados/associados/auth/infra/security/SecurityConfiguration.java
@@ -1,7 +1,7 @@
 package com.associados.associados.auth.infra.security;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,7 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.*;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.associados.associados.auth.filter.SecurityFilter;
 
